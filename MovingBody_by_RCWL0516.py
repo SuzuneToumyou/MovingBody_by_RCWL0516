f0d0752306f48e5f8d54709ca529aacbd4e6dbf9
灯明鈴音 <30884264+SuzuneToumyou@users.noreply.github.com>
2019-03-15 10:09:37 +0900

    Add files via upload

---
+++ MovingBody_by_RCWL0516.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#coding: utf-8
+
+from gpiozero import DigitalInputDevice
+import datetime
+import ambient
+
+from signal import pause
+
+ambi = ambient.Ambient(ID, "writeKey")
+radar = DigitalInputDevice(18, pull_up=False, bounce_time=2.0)
+
+def detector():
+    timestamp = str((datetime.datetime.now()))
+    timestamp = timestamp[0:19]
+    print("Image captured at",timestamp)
+    
+    h=1
+    r = ambi.send({"d1": h})
+    r.close()
+    
+radar.when_activated = detector
+pause()
